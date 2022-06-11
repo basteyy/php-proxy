79e5e9ec299bacd2a65d0be77484eccc4d7a7d0f
basteyy <sebastian@eiweleit.de>
2022-06-11 10:11:56 +0200

    Refactor config

--- src/Config.php
+++ src/Config.php
@@ -1,38 +1,143 @@
 <?php
+/**
+ * PHP-Proxy
+ *
+ * Php-Proxy library to create a web-based proxy
+ * @see https://github.com/Athlon1600/php-proxy
+ * @license MIT
+ * @author https://github.com/Athlon1600/php-proxy/graphs/contributors
+ */
+
+declare(strict_types=1);
 
 namespace Proxy;
 
-// based off of this:
-// http://v3.golaravel.com/api/source-class-Laravel.Config.html#3-235
+use Exception;
+use InvalidArgumentException;
+use function basteyy\VariousPhpSnippets\varDebug;
+
 class Config
 {
-    private static $config = array();
+    /** @var array $configFiles Array which hols the files for loading */
+    private static array $configFiles;
 
-    public static function get($key, $default = null)
+    /** @var string $path The directory where the .env file can be located. */
+    protected string $path;
+
+    /**
+     * Constructor which expect the path to the .env-File
+     * @param string $path
+     * @throws Exception
+     */
+    public function __construct(?string $path = null)
     {
-        return self::has($key) ? static::$config[$key] : $default;
+        if($path) {
+            self::load($path);
+        }
     }
 
-    public static function set($key, $value)
+    /**
+     * Load the .env-File from the path
+     * @see __construct
+     * @return void
+     * @see https://dev.to/fadymr/php-create-your-own-php-dotenv-3k2i
+     * @todo Implement caching
+     */
+    public function putEnv(): void
     {
-        self::$config[$key] = $value;
-    }
+        foreach(self::$configFiles as $file) {
+            $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    public static function has($key)
-    {
-        return isset(static::$config[$key]);
-    }
+            foreach ($lines as $line) {
 
-    public static function load($path)
-    {
-        if (file_exists($path)) {
+                if (str_starts_with(trim($line), '#')) {
+                    continue;
+                }
 
-            // Successful includes, unless overridden by the included file, return 1.
-            $data = require($path);
+                list($name, $value) = explode('=', $line, 2);
+                $name = trim($name);
+                $value = trim($value);
 
-            if (is_array($data)) {
-                self::$config = array_merge(self::$config, $data);
+                if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                    putenv(sprintf('%s=%s', $name, $value));
+
+                    /** Array? */
+                    if(str_contains($value, ',')) {
+                        $value = explode(',',$value);
+                    }
+
+                    $_ENV[$name] = $value;
+                    $_SERVER[$name] = $value;
+                }
             }
         }
+    }
+
+    /**
+     * Getter for fallback (use $_ENV instead)
+     * @param string $name
+     * @param $default
+     * @return mixed
+     */
+    public static function get(
+        string $name,
+        $default = null
+    ) : mixed
+    {
+        return $_ENV[$name] ?? $default;
+    }
+
+    /**
+     * A setter
+     * @param string $name
+     * @param mixed $value
+     * @see putenv()
+     * @return void
+     */
+    public static function set(
+        string $name,
+        mixed $value
+    ): void
+    {
+        putenv(sprintf('%s=%s', $name, $value));
+        $_ENV[$name] = $value;
+        $_SERVER[$name] = $value;
+    }
+
+    /**
+     * Check if a variable exists in the environment
+     * @param string $name
+     * @return bool
+     */
+    public static function has(
+        string $name
+    ): bool
+    {
+        return isset($_ENV[$name]);
+    }
+
+    /**
+     * Add an additional file to the scope
+     * @param string $path
+     * @return void
+     * @throws Exception
+     */
+    public static function load(string $path):void {
+
+        if(is_dir($path)) {
+            $path .='/.env';
+        }
+
+        if(!file_exists($path)) {
+            throw new InvalidArgumentException(sprintf('%s does not exist', $path));
+        }
+
+        if(file_exists($path)) {
+            if(!is_readable($path)) {
+                throw new Exception(sprintf('Config File %s is not readable', $path));
+            }
+        }
+
+        self::$configFiles[] = $path;
     }
 }
